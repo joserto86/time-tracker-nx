fedbd2aa7b7fbc0562c24578d6111359e1ab0538
Iñaki García Blasco <igarcia@irontec.com>
2022-10-11 17:49:24 +0200

    Add @ngrx

--- apps/time-tracker-front/src/app/app.module.ts
+++ apps/time-tracker-front/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material/material.module';
+import { StoreModule } from '@ngrx/store';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })

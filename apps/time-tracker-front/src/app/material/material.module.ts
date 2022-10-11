8a13fb270964bb47cb12a5a043ef06f3c3f41c62
Iñaki García Blasco <igarcia@irontec.com>
2022-10-11 17:07:24 +0200

    Add @angular/material

---
+++ apps/time-tracker-front/src/app/material/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [...modules, CommonModule],
+  exports: modules,
+})
+export class MaterialModule {}

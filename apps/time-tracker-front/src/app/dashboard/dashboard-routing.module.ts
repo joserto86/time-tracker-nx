8115ca43272aa2cfcfa9310090ca7fd0896eed1b
Jose Alberto Hernan <jahernan@irontec.com>
2022-10-14 14:13:27 +0200

    fix import Dashboard Module

--- apps/time-tracker-front/src/app/dashboard/dashboard-routing.module.ts
+++ apps/time-tracker-front/src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../auth/services';
 import { DashboardComponent } from './pages/dashboard.component';
 
 const routes: Routes = [
   { 
-    path: 'dashboard',
+    path: '',
     component: DashboardComponent
   }
 ];
@@ -14,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class DashboardRoutingModule { }

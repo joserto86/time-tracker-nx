6e157d25559a68857e9d18ec0903011d85e63af9
Sergio Peña <spena@irontec.com>
2022-10-17 15:54:53 +0200

    Implemented logout on the front

--- apps/time-tracker-front/src/app/shared/components/layout/layout.component.ts
+++ apps/time-tracker-front/src/app/shared/components/layout/layout.component.ts
@@ -1,16 +1,20 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '../../../auth/actions';
 
 @Component({
   selector: 'time-tracker-nx-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent implements OnInit {
+  constructor(private router: Router, private store: Store) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout(): void {
+    this.store.dispatch(AuthActions.logout());
   }
-
 }

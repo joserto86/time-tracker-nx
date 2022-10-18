38a1235843501d7d0a354a98d14bede9087030f0
Sergio Peña <spena@irontec.com>
2022-10-18 13:57:14 +0200

    Set menu links on sidenav

--- apps/time-tracker-front/src/app/shared/components/layout/layout.component.ts
+++ apps/time-tracker-front/src/app/shared/components/layout/layout.component.ts
@@ -5,6 +5,7 @@ import { AuthActions } from '../../../auth/actions';
 import { PublicUser } from '../../../../../../../libs/src/lib/auth';
 import { Observable } from 'rxjs';
 import * as fromAuth from '../../../auth/reducers/index';
+import { MenuItem } from '../../../../../../../libs/src/lib/sidenav';
 
 @Component({
   selector: 'time-tracker-nx-layout',
@@ -14,6 +15,13 @@ import * as fromAuth from '../../../auth/reducers/index';
 })
 export class LayoutComponent implements OnInit {
   user$: Observable<PublicUser | null | undefined>;
+
+  menuItems: MenuItem[] = [
+    { label: 'Dashboard', path: '/dashboard' },
+    { label: 'Profile', path: '/settings/profile' },
+    { label: 'My filters ', path: '/settings/my-filters' },
+    { label: 'Fill in Gitlab credentials', path: '/settings/credentials' },
+  ];
 
   constructor(private router: Router, private store: Store) {
     this.user$ = this.store.select(fromAuth.selectLoggeduser);

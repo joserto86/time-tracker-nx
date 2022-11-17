a7863dd1452b528eaab533675bb5c93d7d58b43d
Sergio Peña <spena@irontec.com>
2022-11-17 15:02:53 +0100

    Change validation on new filter dialog form

--- apps/time-tracker-front/src/app/settings/components/new-filter-dialog/new-filter-dialog.component.ts
+++ apps/time-tracker-front/src/app/settings/components/new-filter-dialog/new-filter-dialog.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Filter, filterColumns, filterConditions } from '@time-tracker/shared';
 import { Store } from '@ngrx/store';
 import { FilterActions } from '../../state/actions';
+import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'time-tracker-nx-new-filter-dialog',
   templateUrl: './new-filter-dialog.component.html',
@@ -15,6 +16,8 @@ export class NewFilterDialogComponent implements OnInit {
   filterColumns = filterColumns;
   filterConditions = filterConditions;
 
+  formSubscription!: Subscription;
+
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<NewFilterDialogComponent>,
@@ -24,10 +27,14 @@ export class NewFilterDialogComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.fb.group({
       name: ['', [Validators.required]],
-      column: [filterColumns[1], []],
-      condition: [filterConditions[1], []],
-      searchTerm: ['', [Validators.required]],
+      column: ['', [Validators.required]],
+      condition: ['', [Validators.required]],
+      searchTerm: [''],
     });
+
+    this.form.get('searchTerm')?.disable();
+
+    this.checkDisable();
   }
 
   save() {
@@ -45,5 +52,23 @@ export class NewFilterDialogComponent implements OnInit {
 
   close() {
     this.dialogRef.close();
+  }
+
+  checkDisable() {
+    const column = this.form.get('column')?.value;
+    const condition = this.form.get('condition')?.value;
+
+    this.form.get('searchTerm')?.enable();
+
+    if (column === '' || condition === '') {
+      this.form.get('searchTerm')?.disable();
+    }
+
+    if (
+      condition === this.filterConditions[2] ||
+      condition === this.filterConditions[3]
+    ) {
+      this.form.get('searchTerm')?.disable();
+    }
   }
 }
